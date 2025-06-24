bc9a5f66d26373f37af81054e7ddf90c2801729b
martinhild <martin@nimohi.de>
2025-06-24 15:23:35 +0200

    new structure and files

--- Exercises/OOP_Vehicles/Car.cs
+++ Exercises/OOP_Vehicles/Car.cs
@@ -1,7 +1,11 @@
 ﻿
 namespace FQ_Programmieraufgaben.Exercises.OOP_Vehicles
 {
-    internal class Car
+    internal class Car : Vehicle
     {
+        public override void Drive()
+            {
+            Console.WriteLine($"The car is driving at {Speed} km/h.");
+            }
     }
 }
--- Exercises/OOP_Vehicles/Vehicle.cs
+++ Exercises/OOP_Vehicles/Vehicle.cs
@@ -3,5 +3,12 @@ namespace FQ_Programmieraufgaben.Exercises.OOP_Vehicles
 {
     internal class Vehicle
     {
+        // Properties
+        public int Speed { get; set; }
+
+        public virtual void Drive()
+        {
+            Console.WriteLine($"Driving with {Speed} km/h.");
+        }
     }
 }
